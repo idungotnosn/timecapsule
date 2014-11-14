da96469c1c946658a707648929095b4844745f86
Mike Seeley <idungotnosn@gmail.com>
2014-11-13 20:53:54 -0700

    Now have some basic site showing up

--- web.py
+++ web.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+render_template, flash
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, welcome to TimeCapsule!"
+    return render_template('mainsite.html')
 
 if __name__ == "__main__":
     app.run()
