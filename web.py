76602c25f77d3f490615f2c9397fa5ca6185ccfb
Mike Seeley <idungotnosn@gmail.com>
2014-12-01 19:11:28 -0700

    Now can get files from user

--- web.py
+++ web.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def hello():
     return render_template('mainsite.html')
 
+@app.route("/files",methods=['GET','POST'])
+def handleFiles():
+    if request.method == 'GET':
+        print 'Method was GET'
+    if request.method == 'POST':
+        print 'Method was POST'
+        print 'There were '+str(len(request.files))+' files'
+        for fileName in request.files:
+            print fileName
+            currentFile = request.files[fileName]
+            try:
+                for line in currentFile:
+                    print line
+            finally:
+                currentFile.close()
+        return render_template('success.html')
+    return render_template('files.html')
+
 if __name__ == "__main__":
     app.run()
